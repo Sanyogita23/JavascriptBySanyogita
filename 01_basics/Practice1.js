aefb8944516fb1582ea5ce23d50a85eb7e08ae8d
Sanyogita23 <162955374+Sanyogita23@users.noreply.github.com>
2024-03-14 06:05:36 +0000

    practice

--- 01_basics/Practice1.js
+++ 01_basics/Practice1.js
@@ -1,1 +1,48 @@
+let serial_No = 100;   //let const var 
+let name = 'Sanyogita'
+//console.log(name)
+name = 'Arun'  //let and var allows to change the value of variable
+const sub1Marks = 20
+const sub2Marks = 50
 
+//sub1Marks = 30  //cannot change the value of const  it will throw error
+
+
+// console.log(serial_No)
+// console.log(name)
+// console.log(typeof serial_No)
+// console.log(typeof name)
+// console.table([typeof serial_No, typeof name, typeof sub1Marks, typeof sub2Marks])
+// console.table([serial_No, name, sub1Marks, sub2Marks])
+
+// var and let variable
+
+var nameofStudent = 'Sanyogita'
+let nameofParent = 'Shylu'
+console.log(nameofStudent)
+console.log(nameofParent)
+if (nameofStudent = nameofStudent)
+{
+    nameofStudent = 'Aryan'  
+    console.log(nameofStudent)
+    nameofParent = 'Santosh'
+    console.log(nameofParent)
+}
+console.log(nameofStudent)
+console.log(nameofParent)
+
+//Null is a stand alone value 
+var val = null
+console.log(val)
+console.log(typeof val)//type of null is object
+var val1 = undefined
+console.log(val1)
+console.log(typeof val1)
+//type conversion
+
+const aint = 100
+console.log(aint)
+console.log(typeof aint)
+var num = String(aint)  //to string
+console.log(num)
+console.log(typeof num)
