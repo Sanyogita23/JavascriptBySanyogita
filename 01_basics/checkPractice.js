9d25c9f536d1e4f62602210b436f650fb4e6f14a
Sanyogita23 <162955374+Sanyogita23@users.noreply.github.com>
2024-03-20 06:31:01 +0000

    check

---
+++ 01_basics/checkPractice.js
@@ -0,0 +1,49 @@
+let target1 = ['Raja', 'Arun']
+
+
+let myele = target1[0]
+
+//console.log(myele )
+
+let a = myele.split("")
+
+ let b = a
+ //console.log(b )
+// console.log(typeof b )
+
+//reverse function
+
+//console.log(b.reverse())
+//----------------------------------
+var len1 = b.length
+//console.log(len1)
+
+
+let marks = [40, 50, 60, 25, 39, 2, 11]
+
+var a1 = marks[0] >35
+var a2 = marks[1] >35
+var a3 = marks[2] >35
+var a4 = marks[3] >35
+var a5 = marks[4] >35
+var a6 = marks[5] >35
+var a7 = marks[6] >35
+
+console.table([a1, a2, a3, a4, a5, a6, a7])
+
+
+let len = marks.length;
+
+for(i = 0; i<len; i++)
+{
+    var a1 = marks[i] >35
+   
+console.log(`student marks are ${marks[i]} & result is ${a1}`)
+
+
+}
+
+
+
+
+
